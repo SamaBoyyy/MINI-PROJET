810946493be2e36c9cf9849c37dd5687d6411173
Jackno5 <159382039+Jackno5@users.noreply.github.com>
2024-05-03 22:52:36 +0200

    Update test_color.c

--- MINI PROJET/test_color.c
+++ MINI PROJET/test_color.c
@@ -16,16 +16,16 @@ typedef struct grille_s {
 
 grille * Grille_allouer ( unsigned n, unsigned m ){
     int i ;
-    grille *g=NULL;
+    grille *g= malloc(sizeof(grille)); //allocation memoire pour le type grille (reserve d'un espace de taille type grille)
     (g->m) = m;
     (g->n) = n;
-    (g->tab) = malloc(n * sizeof(char *)); 
+    (g->tab) = malloc(n * sizeof(char *)); //alocation memoire de la premiere collonne de taille type pointeur char
     for( i = 0 ; i < m ; i++ ){
-        g->tab[i] = calloc(m, sizeof(char * ));
+        g->tab[i] = calloc(m, sizeof(char * )); //alocation memoire des n ligne de taille m fois type pointeur char
     }
     for( i =  0; i < n  ; i++ ){
         for( int j = 0 ; j < m ; j++ ){
-          *(*(g->tab + i)+j) = malloc(8 * sizeof(char));
+          *(*(g->tab + i)+j) = malloc(8 * sizeof(char)); //alocation memoire qui permet à chaque pointeur du tableau à deux dims de pointer vers un espace de 8 caractere
         }
     }
     return g;
@@ -35,13 +35,13 @@ int main(int argc, char const *argv[])
 {
     printf("H");
     grille *g = Grille_allouer(10, 10);
-    printf("ELLo");
+    printf("ELLo\n");
     for ( int i = 0 ; i < g->n  ; i++ ){
         for( int j = 0 ; j < g->m ; j++ ){
-            *(*(g->tab + i) +j) ="\33[00  ";
+            *(*(g->tab + i) +j) ="\33[42m   \33[00m"; //met la valeur une couleur verte puis l'arrete avec une couleur noir
         }
     }
-    for ( int i = 0 ; i < g->n  ; i++ ){
+    for ( int i = 0 ; i < g->n  ; i++ ){ //affichage
         for( int j = 0 ; j < g->m ; j++ ){
             printf("%s", *(*(g->tab + i) +j));
         }
