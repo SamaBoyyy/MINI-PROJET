b605306a11408953efe1391ffda22035d0674d17
Arsène MAUCOURT <73250969+SamaBoyyy@users.noreply.github.com>
2024-05-08 14:41:42 +0200

    serpent (.h && .c) Liste_Section (.h && .c)
    Remplacement de toutes mension struct maillon par section et struct liste_int par liste_section;
    Modification de la structure Section pour avoir une taille et une couleur à la place d'une valeur;
    Modification de affichage int qui affichait la valeur (useless); affiche la couleur, à modifié quoi que pas très utiles, à voir;
    Ajout d'une structure serpent dans serpent.h

---
+++ MINI PROJET/Liste_Section.h
@@ -0,0 +1,54 @@
+#ifndef _LISTE_INT_H
+#define _LISTE_INT_H
+#define item int
+
+typedef struct section_s {
+  item  taille; /* Taille de la section */
+  char *couleur; /* Couleur section */
+  Section *suivant; /* adresse du maillon suivant */
+}Section;
+
+typedef struct liste_section_s {
+  Section *premier; /* adresse du premier maillon de la liste, NULL si la liste est vide */
+  Section *dernier; /* adresse du dernier maillon de la liste NULL si la liste est vide */
+  int longueur; /* nombre de maillons dans la liste */
+}liste_section;
+
+/* allocation sur le tas pour un maillon de valeur v */
+/* et renvoi de l'adresse du maillon */
+Section *nouvelle_Section (int v);
+
+/* libération de l'espace mémoire occupé par le maillon */
+void detruire_Section (Section **m);
+
+/* allocation sur le tas pour une liste vide */
+/* et renvoi de l'adresse de la liste */
+liste_section *nouvelle_liste_section();
+
+/* test de la vacuité de la liste d'adresse l */
+int est_vide_liste_section(const liste_section *l);
+
+/* renvoi du nombre de maillons dans la liste */
+int longueur_liste_section(const liste_section *l);
+
+/* affichage des valeurs de la liste d'adresse l */
+void afficher_liste_couleur(const liste_section *l);
+
+/* insertion du maillon d'adresse m au début de la liste d'adresse l */
+void ajouter_maillon_debut_liste_section(liste_section *l, Section *m);
+
+/* insertion d'un maillon d'adresse m à la fin de la liste d'adresse l */
+void ajouter_maillon_fin_liste_section(liste_section *l, Section *m);
+
+/* extraction du premier maillon de la liste d'adresse l */
+/* et renvoi de l'adresse du maillon extrait */
+Section *extraire_maillon_debut_liste_section(liste_section *l);
+
+/* extraction du dernier maillon de la liste d'adresse l */
+/* et renvoi de l'adresse du maillon extrait */
+Section *extraire_maillon_fin_liste_section(liste_section *l);
+
+/* libération de l'espace occupé par la liste et tous ses maillons */
+void detruire_liste_section (liste_section **l);
+
+#endif
--- MINI PROJET/serpent.h
+++ MINI PROJET/serpent.h
@@ -1,5 +1,12 @@
 #ifndef SERPENT_H
 #define SERPENT_H
+#include "Liste_Section.h"
 
+
+struct serpent_s{
+    unsigned x;
+    unsigned y;
+    liste_section l_serpent;
+}
 
 #endif
